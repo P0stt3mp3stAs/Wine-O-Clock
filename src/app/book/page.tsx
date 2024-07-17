7545ddf2ccec6a27904947728dead965b23d524c
myMac <elghali@myMacs-iMac.local>
2024-07-17 18:14:38 +0100

    navbar:home, menu, book, pay

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
 
-export default function Home() {
+export default function Book() {
   return (
     <main className="bg-[#1E1E1E] h-screen pt-16">
       <Navbar />
       <div className="flex p-4">
-        <h1 className="text-2xl font-bold">Welcome to Wine-O-Clock</h1>
+        <h1 className="text-2xl font-bold">This is BOOK page of Wine O'Clock</h1>
       </div>
     </main>
   );
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const icons = {
+  home: '/icons/Whome.png',
+  menu: '/icons/Wmenu.png',
+  book: '/icons/Wbook.png',
+  pay: '/icons/Wpay.png',
+  profile: '/icons/wprofile.png',
+};
+
+const pages = [
+  { name: 'Home', path: '/', icon: icons.home },
+  { name: 'Menu', path: '/menu', icon: icons.menu },
+  { name: 'Book', path: '/book', icon: icons.book },
+  { name: 'Pay', path: '/pay', icon: icons.pay },
+];
+
+export default function Navbar() {
+  const currentPath = usePathname();
+
+  return (
+    <nav className="text-white fixed w-full z-10 top-0 flex justify-between items-center px-4 py-1.5">
+      <div className="bg-[#767676] flex space-x-4 h-12 items-center rounded-full py-7 px-1">
+        {pages.map((page) => (
+          <Link href={page.path} key={page.name}>
+            <div className={`flex items-center space-x-2 cursor-pointer rounded-full ${currentPath === page.path ? 'bg-brown' : ''}`} style={{ backgroundColor: currentPath === page.path ? '#5E3200' : 'transparent', padding: '0.5rem' }}>
+              <img src={page.icon} alt={`${page.name} icon`} className="h-8 w-8" />
+              {currentPath === page.path && <span>{page.name}</span>}
+            </div>
+          </Link>
+        ))}
+      </div>
+      <div className="bg-[#767676] rounded-full h-12 w-12 flex items-center justify-center">
+        <img src={icons.profile} alt="Profile icon" className="h-12 w-12 cursor-pointer" />
+      </div>
+    </nav>
+  );
+}
